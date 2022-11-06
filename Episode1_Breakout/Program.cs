5300403bdc79f742f63723afd68621626bba7ebb
Steven <94455238+bymyselfstudio@users.noreply.github.com>
2022-11-06 16:39:31 +0100

    Add files via upload

---
+++ Episode1_Breakout/Program.cs
@@ -0,0 +1,10 @@
+﻿int correctCode = 0;
+int maxPositions = 100;
+int currentNumber = 0;
+
+while (currentNumber <= maxPositions)
+{
+    if (correctCode > maxPositions) { Console.WriteLine("Incorrect input!"); break; }
+    if (currentNumber == correctCode) { Console.WriteLine("Opened lock with code " + currentNumber); break; }
+    currentNumber++;
+}
